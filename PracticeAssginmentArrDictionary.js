6354c036524b0ba3b51577fcc3be493bd2d232d8
Aneesh <katta.aneesh597@gmail.com>
2022-12-29 15:59:51 +0530

    added program

--- PracticeAssginmentArrDictionary.js
+++ PracticeAssginmentArrDictionary.js
@@ -2,13 +2,18 @@ let array = new Array(10);
 let min = 100;
 let max = 999;
 for (i = 0; i < array.length; i++) {
-    array[i] =Math.floor(Math.random() * (max - min)) + min; 
+    array[i] = Math.floor(Math.random() * (max - min)) + min;
 }
 for (i = 0; i < array.length; i++) {
-    console.log("At index: "+i+" value: "+array[i]);
+    console.log("At index: " + i + " Generated three digit value: " + array[i]);
 }
-let storeArray=array;
+let storeArray = array;
 for (i = 0; i < storeArray.length; i++) {
-    console.log("At index: "+i+" storeArray value: "+storeArray[i]);
+    console.log("At index: " + i + " storeArray before sort value: " + storeArray[i]);
 }
-
+let xarray = storeArray.slice(0).sort();
+for (i = 0; i < xarray.length; i++) {
+    console.log("At index: " + i + " after sort value: " + xarray[i]);
+}
+console.log("Second Largest " + xarray[xarray.length-2]);
+console.log("Second smallest " + xarray[1]);
