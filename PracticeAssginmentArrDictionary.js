83990faa26db397e1371224c7c9f59183a1afea4
Aneesh <katta.aneesh597@gmail.com>
2023-01-02 17:20:59 +0530

    added program to sort the array and then find the 2nd largest
    and the 2nd smallest element.

--- PracticeAssginmentArrDictionary.js
+++ PracticeAssginmentArrDictionary.js
@@ -1,9 +1,10 @@
-let array = new Array(10);
+let array = new Array(9);
 let min = 100;
 let max = 999;
 for (i = 0; i < array.length; i++) {
     array[i] = Math.floor(Math.random() * (max - min)) + min;
 }
+//storeArray=[1,3,8,6,4,7,8,9,9];
 for (i = 0; i < array.length; i++) {
     console.log("At index: " + i + " Generated three digit value: " + array[i]);
 }
@@ -11,9 +12,23 @@ let storeArray = array;
 for (i = 0; i < storeArray.length; i++) {
     console.log("At index: " + i + " StoreArray before sort value: " + storeArray[i]);
 }
-let xarray = storeArray.slice(0).sort();
-for (i = 0; i < xarray.length; i++) {
-    console.log("At index: " + i + " After sort value: " + xarray[i]);
+let numbers=storeArray;
+SecondLargestNSecondSmallest(numbers);
+
+function SecondLargestNSecondSmallest(numbers)
+{
+  
+  console.log(""+numbers.sort((a, b) => a - b));
+  let size=numbers.length;
+  console.log("Array size:"+(size+1));
+  for(let i=size-2;i>=0;i--)
+  {
+    if(numbers[i]!=numbers[size-1])
+    {
+      console.log("the second largest element is "+ numbers[i]);
+      console.log("the second smallest element is "+ numbers[1]);
+      return;
+    }
+  }
+  console.log("there is no second largest element");
 }
-console.log("Second Largest " + xarray[xarray.length-2]);
-console.log("Second smallest " + xarray[1]);
