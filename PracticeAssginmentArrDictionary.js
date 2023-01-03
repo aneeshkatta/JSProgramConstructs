0e0e16bd0f7e44cfeb3445f1b56550fdb1b4c831
Aneesh <katta.aneesh597@gmail.com>
2023-01-03 10:58:33 +0530

    added program

--- PracticeAssginmentArrDictionary.js
+++ PracticeAssginmentArrDictionary.js
@@ -1,16 +1,15 @@
-var prompt =require('prompt-sync')();
-const num = prompt("What is your number for prime factorization? ");
-let array=new Array(num);
-let store= new Array();
+// var prompt =require('prompt-sync')();
+// const num = prompt("What is your number for prime factorization? ");
+let num=24;
+var x=parseInt(num);
+let array=new Array();
+let store=array;
 
-for (let i = 0; i <= num; i++) {
-    if (num % i == 0) {
-        array[i] = i;  
+for (let i = 0; i < x; i++) {
+    if (num % i == 0 && num%i!=undefined) {
+        array.push(i);  
       
     }  
-    for (let i = 0; i <= store.length; i++) {
-        if(array!=undefined)
-    store=array; }  
 }
-for (let i = 0; i <= store.length; i++) {
+for (let i = 0; i < store.length; i++) {
 console.log(""+store[i]);}
