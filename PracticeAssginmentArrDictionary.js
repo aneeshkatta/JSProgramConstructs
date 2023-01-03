11975ad0b85952e8d7c8fd7303f14977e3fad673
Aneesh <katta.aneesh597@gmail.com>
2023-01-03 13:58:49 +0530

    added program

--- PracticeAssginmentArrDictionary.js
+++ PracticeAssginmentArrDictionary.js
@@ -3,5 +3,12 @@ let ran = Math.floor(Math.random() * (6 - 1) + 1)
 console.log(ran);//1a.roll the dice
 for (i = 0; i < 10; i++) {//1b.repeat die roll
   let ran = Math.floor(Math.random() * (6 - 1) + 1)
-  console.log(ran);
+  //console.log(ran);
+}
+for (i = 0; i < 10; i++) {//1c.store repeat values in dictionary
+  let ran = Math.floor(Math.random() * (6 - 1) + 1)
+  key = i;
+  value = ran;
+  map.set[key, value];
+ console.log("Key" + key + "Value" + value);
 }
